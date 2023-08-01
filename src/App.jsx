65176c23bc0325356389493befab58e9ff058be0
imannfazal <imannfazal@gmail.com>
2023-08-01 17:06:47 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { createBrowserRouter } from 'react-router-dom';
+
+const router= createBrowserRouter([
+  {
+    path: '/',
+    element: <h1>Home</h1>
+  },
+  {
+    path: '/about',
+    element: <h1>About Page</h1>
+  },
+]);
 
 const App = () => {
   return (
-    <h1>Jobify App</h1>
-  )
+    <RouterProvider router={router} />
+  );
 }
 
 export default App;
