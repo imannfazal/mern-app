98521e2dddcb2e45f670c8f44ad1de24451106b4
imannfazal <imannfazal@gmail.com>
2023-08-02 14:22:23 +0500

    Update Landing.jsx

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -3,17 +3,21 @@ import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
 const Landing = () => {
-  const StyledBtn = styled.button`
-  font-size: 1.5rem;
-  background:red;
-  color:white;
-  `
   return (
-    <div>
+    <Wrapper>
       <h5>Already have an account? <Link to="/login">Login</Link> or create a new account! <Link to="/register">Signup</Link></h5>
-      <br/><StyledBtn>Styled Button</StyledBtn>
-    </div>
+      <br/><div className='content'>Styled Button</div>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  background:red;
+  color:white;
+  .content{
+    background:blue;
+    color: violet;
+  }
+  `;
 
 export default Landing
