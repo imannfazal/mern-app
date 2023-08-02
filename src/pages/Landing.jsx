d634f73258f6376314bd5c787eddcae8a0680765
imannfazal <imannfazal@gmail.com>
2023-08-02 14:14:42 +0500

    styled cmps

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 
 const Landing = () => {
+  const StyledBtn = styled.button`
+  font-size: 1.5rem;
+  background:red;
+  color:white;
+  `
   return (
-    <div><h5>Already have an account? <Link to="/login">Login</Link> or create a new account! <Link to="/register">Signup</Link></h5></div>
+    <div>
+      <h5>Already have an account? <Link to="/login">Login</Link> or create a new account! <Link to="/register">Signup</Link></h5>
+      <br/><StyledBtn>Styled Button</StyledBtn>
+    </div>
   )
 }
 
