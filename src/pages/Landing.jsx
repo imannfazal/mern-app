c493d9281b089bf1311cd7d736ee721a6e70c967
imannfazal <imannfazal@gmail.com>
2023-08-02 23:43:26 +0500

    landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,23 +1,33 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
+import Wrapper from '../assets/wrappers/LandingPage'
+import logo from '../assets/images/logo.svg'
+import main from '../assets/images/main.svg'
 
 const Landing = () => {
   return (
     <Wrapper>
-      <h5>Already have an account? <Link to="/login">Login</Link> or create a new account! <Link to="/register">Signup</Link></h5>
-      <br/><div className='content'>Styled Button</div>
+      <nav>
+        <img src={logo} alt='jobify' className='logo' />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking </span>App
+          </h1>
+          <p>
+            I'm baby jean shorts lyft bicycle rights taxidermy franzen venmo. Intelligentsia normcore same franzen ascot shabby chic, godard neutral milk hotel vegan farm-to-table viral bitters gentrify pabst kogi. Chicharrones jawn vape, master cleanse hexagon enamel pin meditation taiyaki neutra fashion axe wayfarers gatekeep celiac. Hashtag praxis shoreditch yuccie locavore, air plant fashion axe XOXO fixie truffaut hell of slow-carb.
+          </p>
+          <Link to='/register' className='btn register-link'>Register</Link>
+          <Link to='/login' className='btn'>Login / Demo user</Link>
+        </div>
+        <img src={main} className='img main-img' />
+      </div>
     </Wrapper>
   )
 }
 
-const Wrapper = styled.div`
-  background:red;
-  color:white;
-  .content{
-    background:blue;
-    color: violet;
-  }
-  `;
 
-export default Landing
+
+export default Landing;
