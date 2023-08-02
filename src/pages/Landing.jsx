c842c149ca0c1f85174f419c04a37063ab4c9cec
imannfazal <imannfazal@gmail.com>
2023-08-03 00:04:10 +0500

    logo cmp

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import styled from 'styled-components'
 import Wrapper from '../assets/wrappers/LandingPage'
-import logo from '../assets/images/logo.svg'
 import main from '../assets/images/main.svg'
+import Logo from '../components/Logo'
 
 const Landing = () => {
   return (
     <Wrapper>
       <nav>
-        <img src={logo} alt='jobify' className='logo' />
+        <Logo />
       </nav>
       <div className="container page">
         <div className="info">
