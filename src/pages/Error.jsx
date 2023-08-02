b2c3fd9a3195a03c0f7568f6f81231233082501b
imannfazal <imannfazal@gmail.com>
2023-08-03 00:44:27 +0500

    Update Error.jsx

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -13,7 +13,7 @@ const Error = () => {
           <img src={img} alt='not found' />
           <h3>Ohh! page not found </h3>
           <p>We can't seem to find the page you're looking for :{'('} </p>
-          <Link to='/dashboard'>Back Home</Link>
+          <a href='/dashboard'>Back Home</a>
         </div>
       </Wrapper>
     )
@@ -22,7 +22,7 @@ const Error = () => {
     <Wrapper>
       <div>
       <h3>Something went wrong!</h3>
-      <h5><Link to='/'>Back Home</Link></h5>
+      <h5><a href='/dashboard'>Back Home</a></h5>
       </div>
     </Wrapper>
   )
