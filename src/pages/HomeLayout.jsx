2f7d7acbf38a751782cfb93fa9008056b8380f4c
imannfazal <imannfazal@gmail.com>
2023-08-02 11:54:58 +0500

    link

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const HomeLayout = () => {
   return (
-    <h1>Home Layout</h1>
+    <>
+      <h1>Home Layout</h1><br />
+      <h5>Already have an account? <Link to="/login">Login</Link> or create a new account! <Link to="/register">Signup</Link></h5>
+      
+    </>
   )
 }
 
