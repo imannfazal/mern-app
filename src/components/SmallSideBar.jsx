86b7f74c70772c8730d2f1fe5f40bbb2c8da66d5
imannfazal <imannfazal@gmail.com>
2023-08-03 23:56:34 +0500

    Create SmallSideBar.jsx

---
+++ src/components/SmallSideBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Wrapper from '../assets/wrappers/SmallSidebar'
+
+const SmallSideBar = () => {
+  return (
+    <Wrapper>SmallSideBar</Wrapper>
+  )
+}
+
+export default SmallSideBar
