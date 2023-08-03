a0ec0a10132d20d3a60bd8f6f7b195dd9704b658
imannfazal <imannfazal@gmail.com>
2023-08-03 15:13:28 +0500

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage'
+import Logo from '../components/Logo'
+import FormRow from '../components/FormRow'
+import { Link } from 'react-router-dom'
+
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <Wrapper>
+      <form className='form'>
+        <Logo />
+        <h4>login</h4>
+        <FormRow name='email' type='email' defaultValue='iman@gmail.com' />
+        <FormRow name='password' type='password' defaultValue='secret123' />
+        <button type='submit' className='btn btn-block'>Login</button>
+        <button type='button' className='btn btn-block'>Explore the app</button>
+        <p>
+          Not a member yet?
+          <Link to='/register' className='member-btn'>Register</Link>
+        </p>
+      </form>
+    </Wrapper>
   )
 }
 
