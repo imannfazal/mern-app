8a729fa5ae456761d5f446fb67376726290d6ee2
imannfazal <imannfazal@gmail.com>
2023-08-04 00:35:39 +0500

    Update DashboardLayout.jsx

--- src/pages/DashboardLayout.jsx
+++ src/pages/DashboardLayout.jsx
@@ -1,25 +1,47 @@
-import React from 'react'
+import React, { createContext, useContext, useState } from 'react'
 import Wrapper from '../assets/wrappers/Dashboard';
 import BigSideBar from '../components/BigSideBar';
 import SmallSideBar from '../components/SmallSideBar';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 
+const DashboardContext = createContext();
+
 const DashboardLayout = () => {
+  //temp
+  const user = { name: 'iman' };
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar);
+  }
+
+  const toggleDarkTheme = () => {
+    console.log('set dark theme');
+  }
+
+  const logoutUser = async () => {
+    console.log('logout user')
+  }
+
   return (
-    <Wrapper>
-      <main className="dashboard">
-        <BigSideBar />
-        <SmallSideBar />
-        <div>
-          <Navbar />
-          <div className="dashboard-page">
-            <Outlet />
+    <DashboardContext.Provider value={{user,showSidebar,isDarkTheme,toggleDarkTheme,toggleSidebar,logoutUser}}>
+      <Wrapper>
+        <main className="dashboard">
+          <BigSideBar />
+          <SmallSideBar />
+          <div>
+            <Navbar />
+            <div className="dashboard-page">
+              <Outlet />
+            </div>
           </div>
-        </div>
-      </main>
-    </Wrapper>
+        </main>
+      </Wrapper>
+    </DashboardContext.Provider>
   )
 }
 
+export const useDashboardContext = useContext(DashboardContext);
 export default DashboardLayout
