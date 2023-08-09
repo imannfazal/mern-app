3f39b333c1bb96fe8a9947593b5d30348947424c
imannfazal <imannfazal@gmail.com>
2023-08-09 13:06:05 +0500

    icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import Wrapper from '../assets/wrappers/Navbar'
-import { FaHome } from 'react-icons/fa'
+import { FaAlignLeft } from 'react-icons/fa'
+import Logo from './Logo';
 
 const Navbar = () => {
   return (
     <Wrapper>
-      <h2>Navbar</h2>
-      <FaHome />
+      <div className="nav-center">
+        <button type='button' className='toggle-btn'>
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h4 className='logo-text'>Dashboard</h4>
+        </div>
+        <div className="button-container">
+          toggle/logout
+        </div>
+      </div>
     </Wrapper>
   )
 }
