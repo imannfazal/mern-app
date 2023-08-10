7a00fe248ccca15bdfc2066e2be089b6e24f6124
imannfazal <imannfazal@gmail.com>
2023-08-10 23:40:29 +0500

    Update links.jsx

--- src/utils/links.jsx
+++ src/utils/links.jsx
@@ -4,3 +4,9 @@ import { MdQueryStats } from 'react-icons/md';
 import { FaWpforms } from 'react-icons/fa';
 import { ImProfile } from 'react-icons/im';
 import { MdAdminPanelSettings } from 'react-icons/md';
+
+const links = [
+    {
+        text: 'add job'
+    }
+]
